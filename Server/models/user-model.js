7632f40e4c263e469ed0ddd5066402b4bfb01020
anirudhhbehera <anirudhhbehera@gmail.com>
2024-02-14 22:22:11 +0530

    added jwt token feature for future authentication

--- Server/models/user-model.js
+++ Server/models/user-model.js
@@ -1,5 +1,7 @@
 // Importing the mongoose module for MongoDB object modeling
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");//importing jwt
+
 
 // Defining the schema for the User collection
 const userSchema = new mongoose.Schema({
@@ -25,6 +27,25 @@ const userSchema = new mongoose.Schema({
         default: false,
     }
 });
+
+userSchema.methods.generateToken = async function () {
+    console.log("I am token");
+    try {
+      return jwt.sign(
+        {
+          userId: this._id.toString(),
+          email: this.email,
+          isAdmin: this.isAdmin,
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+          expiresIn: "30d",
+        }
+      );
+    } catch (error) {
+      console.error("Token Error: ", error);
+    }
+  };
 
 // Creating a model named "User" based on the userSchema
 const User = new mongoose.model("User", userSchema);
