be16c50b8ae2ecf315e28a92d3d893d976b1a83b
anirudhhbehera <anirudhhbehera@gmail.com>
2024-02-27 02:57:46 +0530

    Added the images for styling

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,4 +1,33 @@
-export const Register=()=>{
-    return(
-    <h1>Hello register page</h1>)
-}
+
+export const Register = () => {
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="a girl is trying to do registration"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">registration form</h1>
+                <br />
+                <form>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input type="text" name="" />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+};
