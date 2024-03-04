b142d4280a7fddc90860707ab083a1e4e87a647f
anirudhhbehera <anirudhhbehera@gmail.com>
2024-03-04 23:13:44 +0530

    Added Registration logic on frontend

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
 
 export const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  // handle form on submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
     <>
       <section>
@@ -17,11 +41,50 @@ export const Register = () => {
               <div className="registration-form">
                 <h1 className="main-heading mb-3">registration form</h1>
                 <br />
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div>
                     <label htmlFor="username">username</label>
-                    <input type="text" name="" />
+                    <input
+                      type="text"
+                      name="username"
+                      value={user.username}
+                      onChange={handleInput}
+                      placeholder="username"
+                    />
                   </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      value={user.email}
+                      onChange={handleInput}
+                      placeholder="email"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      value={user.phone}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={user.password}
+                      onChange={handleInput}
+                      placeholder="password"
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
                 </form>
               </div>
             </div>
